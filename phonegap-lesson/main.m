ebef531d84db023d677a63527218ac7ea8ef2557
Yoshihito Kuranuki <kuranuki@sonicgarden.jp>
2012-01-28 14:09:56 +0900

    Initial Commit

---
+++ phonegap-lesson/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  phonegap-lesson
+//
+//  Created by 義人 倉貫 on 12/01/28.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
